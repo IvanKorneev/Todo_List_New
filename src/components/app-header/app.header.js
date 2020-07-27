c2d9e291fba9c762a3d5f2cdfb387511ca7d78ed
Ivan <bomak54@gmail.com>
2020-07-27 12:54:38 +0300

    addComponents

---
+++ src/components/app-header/app.header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './app-header.css'
+const AppHeader = () => {
+    return (
+        <div className="app-header d-flex">
+            <h1>Ivan </h1>
+            <h2>5post </h2>
+        </div>
+    )
+}
+export default AppHeader;
